4eb68472865dcc9a5336d2f66aeb640fec47a27f
martin temoinko <martin_temoinko@hotmail.com>
2022-09-29 12:41:37 -0300

    navbar modificado

--- PreEntrega1Temoinko/preentrega1temoinko/src/components/Brand.js
+++ PreEntrega1Temoinko/preentrega1temoinko/src/components/Brand.js
@@ -1,7 +1,14 @@
+
+import React, { Component } from "react";
 const Brand = (props) =>{
-    
+
+   
+       
     return (
+        <>
+       
         <h1>{props.marca}</h1>
+        </>
     )
 }
 export default Brand;
